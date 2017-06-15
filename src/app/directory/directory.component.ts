2b8aa0cfaa026897c3142ac6ef342407964159a9
aayanqazi <aayanqazi@yahoo.com>
2017-06-16 03:45:26 +0500

    Event Binding And Directive Introduction

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css']
 })
 export class DirectoryComponent implements OnInit {
-  ninja:string;
+  ninja: string;
+  classess: object;
   constructor(private route: ActivatedRoute) {
     this.ninja = route.snapshot.params['id'];
-       console.log("Ninja World " ,this.ninja)
+    console.log("Ninja World ", this.ninja)
+    this.classess = {'blue':true,'underline':false,'red':false};
 
-   }
+  }
 
+  red() {
+    this.classess = {'blue':false,'underline':false,'red':true};
+  }
 
+blue() {
+    this.classess = {'blue':true,'underline':false,'red':false};
+  }
+  underline() {
+    this.classess = {'blue':true,'underline':true,'red':false};
+  }
   ngOnInit() {
   }
 
